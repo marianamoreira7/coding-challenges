52062071561a0dca37e84ad699df8553570aaa08
marianamoreira7 <maribrignol.98@gmail.com>
2026-02-10 16:52:14 -0300

    Amazon Server exercise

---
+++ src/amazon_servers/Main.java
@@ -0,0 +1,25 @@
+package amazon_servers;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+       Solution sol = new Solution();
+
+        System.out.println("Caso 1");
+        System.out.println(Arrays.toString(sol.serverId(5, 5, new int[]{3, 2, 3, 2, 4})));
+        System.out.println("Resposta");
+        System.out.println(Arrays.toString(new int[]{0, 1, 2, 0, 3}));
+
+        System.out.println("Caso 2");
+        System.out.println(Arrays.toString(sol.serverId(5, 4, new int[]{4, 0, 2, 2})));
+        System.out.println("Resposta");
+        System.out.println(Arrays.toString(new int[]{0, 0, 1, 2}));
+
+        System.out.println("Caso 3");
+        System.out.println(Arrays.toString(sol.serverId(5, 4, new int[]{0, 1, 2, 3})));
+        System.out.println("Resposta");
+        System.out.println(Arrays.toString(new int[]{0, 1, 2, 3}));
+    }
+}
---
+++ src/amazon_servers/Solution.java
@@ -0,0 +1,28 @@
+package amazon_servers;
+
+public class Solution {
+
+    public int[] serverId (int numServers, int n, int[] request) {
+        int[] serverIds = new int[n];
+        int[] serverAllocation = new int[numServers];
+
+        for(int i=0;i<n;i++){
+            int minServer = serverAllocation[0];
+            int minAllocationId=0;
+
+            for(int j=0;j<=request[i];j++){
+                if(serverAllocation[j]<minServer) {
+                    minServer = serverAllocation[j];
+                    minAllocationId=j;
+                }
+            }
+
+            serverIds[i]=minAllocationId;
+            serverAllocation[minAllocationId]++;
+
+        }
+        
+        return serverIds;
+    }
+
+}
